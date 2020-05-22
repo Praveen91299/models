0a56b70f34451feb19b96967a1364afbb2ff62a5
Praveen Jayakumar <35169492+Praveen91299@users.noreply.github.com>
2020-05-23 03:55:16 +0530

    Zombie angle correction
    Corrected the issues with the previous push. Behaving properly now.

--- update_position_zombie.m
+++ update_position_zombie.m
@@ -16,6 +16,7 @@ function [X_new] = update_position_zombie(X, z, h, bound)
                 if (dist < dist_min) && (X(j,3) ==0)
                     dist_min = dist;
                     nei = X(j,:);
+                    X_new(i, 7) = j;
                 end
             end
             %{
@@ -34,24 +35,23 @@ function [X_new] = update_position_zombie(X, z, h, bound)
             %}
         end
         
-        %thus now we have x_cent, y_cent
+        %thus now we calculate x_cent, y_cent
         if ~isempty(nei) && X(i,3) == 0
             x_cent = sum(gaussian(distance(X(i,1:2),nei(:,1:2)), 0, z.radius/2).*nei(:,1))/sum(gaussian(distance(X(i,1:2),nei(:,1:2)), 0, z.radius/2));
             y_cent = sum(gaussian(distance(X(i,1:2),nei(:,1:2)), 0, z.radius/2).*nei(:,2))/sum(gaussian(distance(X(i,1:2),nei(:,1:2)), 0, z.radius/2));
-            theta = atan((y_cent - X(i,2))/(x_cent - X(i,1)));
+            theta = atan2((y_cent - X(i,2)), (x_cent - X(i,1)));
         elseif ~isempty(nei) && X(i,3) == 1 && X(i,6) == 0
-            theta = atan((nei(1,2) - X(i,2))/(nei(1,1) - X(i,1)));
+            theta = atan2((nei(1,2) - X(i,2)),(nei(1,1) - X(i,1)));
             X_new(i,6) = 1;
-            X_new(i,7) = theta;
-        elseif ~isempty(nei) && X(i,3) == 1 && X(i,6) == 1
-            theta = X(i,7);
+        elseif X(i,3) == 1 && X(i,6) == 1
+            theta = atan2(X(X(i,7),2) - X(i,2), X(X(i,7),1) - X(i,1));
         else
             theta = sum(2*pi*rand(2),'all');
             X_new(i,7) = 0;
             X_new(i,6) = 0;
         end
         
-        
+        %movement update
         if X_new(i,3) == 0
             if X_new(i,4) == 1
                 X_new(i,1) = X_new(i,1) - h.speed_sprint*cos(theta);
@@ -65,6 +65,7 @@ function [X_new] = update_position_zombie(X, z, h, bound)
             X_new(i,2) = X_new(i,2) + z.speed*sin(theta);
         end
         
+        %Boundary check and reflection
         if (X_new(i,1) > bound.x_max)
             X_new(i,1) = 2*bound.x_max - X_new(i,1);
         end
